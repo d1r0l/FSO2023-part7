7727c855b11b56330ce48df7acce8563208864b1
d1r0l <xd1r0lx@gmail.com>
2023-07-27 17:33:06 +0400

    readme fix

--- countries_hook/src/App.js
+++ countries_hook/src/App.js
@@ -1,144 +1,71 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from 'react'
-import countriesService from './components/countries'
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const CountryNotFound = () => {
-  return (
-    <div>Country not found, specify another filter.</div>
-  )
+const useField = (type) => {
+  const [ value, setValue ] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
 }
 
-const OneCountryFound = ({ countriesAll, filteredCountriesNames }) => {
-  const [ weather, setWeather ] = useState([])
-  const country = countriesAll[filteredCountriesNames[0].id]
-  const languages = Object.values(country.languages)
+const useCountry = (name) => {
+  const [ country, setCountry ] = useState(null)
 
-  useEffect(() => {
-    countriesService
-      .getWeather(country.capital, country.cca2)
-      .then((response) => setWeather(response))
-      .catch(() => {
-        alert('An error occured connecting to weather server')
-      })
-  }, [ country ])
+  useEffect(() => {})
 
-  return (
-    <div>
-      <h1>{country.name.common}</h1>
-      <p>
-        Capital: {country.capital}
-        <br />
-        Area: {country.area}
-      </p>
-      <p>Spoken languages:</p>
-      <ul>
-        {languages.map((language) => (
-          <li key={languages.indexOf(language)}>{language}</li>
-        ))}
-      </ul>
-      <img src={country.flags.png} alt="Country flag"></img>
-      <h2>Weather in {country.capital}</h2>
-      <p>Temperature: {weather.temp} Celcius</p>
-      <img src={weather.icon} alt="Weather pic"></img>
-      <p>Wind: {weather.wind} m/s</p>
-    </div>
-  )
+  return country
 }
 
-const SeveralCountriesFound = ({ filteredCountriesNames, buttonValue }) => {
-  const handleClick = (filterName) => buttonValue(filterName)
-  return filteredCountriesNames.map((country) => (
-    <div key={country.id}>
-      {country.displayedName}
-      <button onClick={() => handleClick(country.filterName)}>Show</button>
-    </div>
-  ))
-}
-
-const ALotOfCountriesFound = () => {
-  return <div>Too many matches, specify another filter.</div>
-}
-
-const CountryInfo = ({ countriesAll, filteredCountriesNames, filter, buttonValue }) => {
-  const Qty = filteredCountriesNames.length
-  if (countriesAll.length === 0 || filter === '') {
+const Country = ({ country }) => {
+  if (!country) {
     return null
-  } else {
-    if (Qty === 0) {
-      return <CountryNotFound />
-    } else if (Qty === 1) {
-      return (
-        <OneCountryFound
-          countriesAll={countriesAll}
-          filteredCountriesNames={filteredCountriesNames}
-        />
-      )
-    } else if (Qty > 1 && Qty <= 10) {
-      const handleClick = (filterName) => buttonValue(filterName)
-      return (
-        <SeveralCountriesFound
-          filteredCountriesNames={filteredCountriesNames}
-          buttonValue={handleClick}
-        />
-      )
-    } else {
-      return <ALotOfCountriesFound />
-    }
-  }
-}
-
-function App() {
-  const [ filter, setFilter ] = useState('')
-  const [ countriesAll, setCountriesAll ] = useState([])
-
-  useEffect(() => {
-    countriesService
-      .getAll()
-      .then((response) => {
-        setCountriesAll(response)
-      })
-      .catch(() => {
-        alert('An error occured connecting to server')
-      })
-  }, [])
-
-  const handleChangeFilter = (event) => {
-    setFilter(event.target.value)
   }
 
-  const handleClick = (filterName) => {
-    setFilter(filterName)
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
   }
-
-  const countriesNamesConcatenated = countriesAll.map((country, index) => ({
-    name: country.altSpellings
-      .concat(
-        country.name.common,
-        country.name.official,
-        Object.values(country.translations).map((item) => item.common),
-        Object.values(country.translations).map((item) => item.official)
-      )
-      .join(', '),
-    id: index,
-    displayedName: country.name.common,
-    filterName: country.name.common.toLowerCase(),
-    cca2: country.cca2,
-    capital: country.capital,
-  }))
-
-  const filteredCountriesNames = countriesNamesConcatenated.filter((country) =>
-    country.name.toLowerCase().includes(filter.toLowerCase())
-  )
 
   return (
     <div>
-      Find countries: <input onChange={handleChangeFilter} />
-      <CountryInfo
-        countriesAll={countriesAll}
-        filteredCountriesNames={filteredCountriesNames}
-        filter={filter}
-        buttonValue={handleClick}
-      />
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [ name, setName ] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
     </div>
   )
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # This repository contains results of exercises of part 7 of Full Stack Open 2023 performed by me
 
-## Folder "anecdotes_routed" contains exercises 7.1-7.3
+## Folder "anecdotes_routed" contains exercises 7.1-7.6
+
+## Folder "countries_hook" contains exercises 7.7-7.x
