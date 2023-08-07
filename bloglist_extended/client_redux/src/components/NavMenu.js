a659134c83cedf7f117a159d5bcf354ea92a2ae3
d1r0l <xd1r0lx@gmail.com>
2023-08-07 15:56:11 +0400

    fixed a Nav menu styling

--- bloglist_extended/client_redux/src/components/NavMenu.js
+++ bloglist_extended/client_redux/src/components/NavMenu.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { logoutUser } from '../reducers/activeUserReducer'
-import { Grid, Button, Stack, Typography, Box } from '@mui/material'
+import { Grid, Button, Stack, Typography } from '@mui/material'
 
 const NavMenu = () => {
   const activeUser = useSelector(state => state.activeUser)
@@ -9,34 +9,36 @@ const NavMenu = () => {
   const dispatch = useDispatch()
 
   return (
-    <Grid container mb={2}>
-      <Grid item xs={6} sm={3}>
-        <Stack direction='row' spacing={1}>
-          <Button component={Link} to='/' variant='contained'>
-            Blogs
-          </Button>
-          <Button component={Link} to='/users' variant='contained'>
-            Users
-          </Button>
-        </Stack>
+    <Grid
+      container
+      sx={{
+        mb: 2,
+        justifyContent: 'space-between'
+      }}
+      spacing={1}
+    >
+      <Grid item>
+        <Grid container>
+          <Stack direction='row' spacing={1}>
+            <Button component={Link} to='/' variant='contained'>
+              Blogs
+            </Button>
+            <Button component={Link} to='/users' variant='contained'>
+              Users
+            </Button>
+          </Stack>
+        </Grid>
       </Grid>
-      <Grid item xs={6} sm={9}>
+      <Grid item>
         {activeUser ? (
-          <Stack
-            direction='row'
-            spacing={1}
-            alignItems='center'
-            justifyContent='flex-end'
-          >
-            <Box>
-              <Typography
-                variant='caption'
-                sx={{ display: { xs: 'none', sm: 'block' } }}
-                noWrap
-              >
-                {activeUser.name} logged in
-              </Typography>
-            </Box>
+          <Stack direction='row' alignItems='center'>
+            <Typography
+              variant='caption'
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+              noWrap
+            >
+              {activeUser.name} logged in &nbsp;&nbsp;&nbsp;
+            </Typography>
             <Button
               type='button'
               onClick={() => dispatch(logoutUser())}
